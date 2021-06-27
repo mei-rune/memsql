88b15d5f44495cc8be7c0b8890f5d8ad4842205c
meifakun <runner.mei@gmail.com>
2021-06-27 18:29:26 +0800

    add storage

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/runner-mei/memsql
 
 go 1.11
+
+require github.com/xwb1989/sqlparser v0.0.0-20180606152119-120387863bf2 // indirect
---
+++ parser.go
@@ -0,0 +1,20 @@
+package memsql
+
+import (
+	"errors"
+
+	"github.com/xwb1989/sqlparser"
+)
+
+func parse(sqlstr string) (*sqlparser.Select, error) {
+	stmt, err := sqlparser.Parse(sqlstr)
+	if err != nil {
+		return nil, err
+	}
+	// Otherwise do something with stmt
+	selectStmt, ok := stmt.(*sqlparser.Select)
+	if !ok {
+		return nil, errors.New("only support select statement")
+	}
+	return selectStmt, nil
+}
